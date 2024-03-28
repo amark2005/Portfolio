55e05a5dd8726dc0a2eb6174a2e8f8c69290b37c
Amar <amarkeshav14@gmail.com>
2024-03-29 00:09:39 +0530

    Hire me button added and functioned

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-import requests
-import json
-app=Flask(__name__, static_folder="/portfolio/style.css")
-@app.route('/')
-def index():
-    return render_template("home.html")
-app.run(host="0.0.0.0",port=5002,debug=True)
---
+++ script.js
@@ -0,0 +1,3 @@
+function func(){
+    window.open("https://www.linkedin.com/in/amar-keshav-735176204",'_blank');
+}
