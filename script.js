cd6ee229e28c076bf33c070e1a4af3b0d7c3c92e
Amar <amarkeshav14@gmail.com>
2024-03-29 17:14:28 +0530

    almost done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function func(){
     window.open("https://www.linkedin.com/in/amar-keshav-735176204",'_blank');
 }
+
+function x(){
+    window.open("https://x.com/amark1412?t=Xvj7r4NtZMllQZjDPSM-Q&s=09",'_blank')
+}
+function insta(){
+    window.open("https://www.instagram.com/retr0.14",'_blank')
+}function whatsapp(){
+    window.open("https://api.whatsapp.com/send?phone=919894458144&text=",'_blank')
+}
